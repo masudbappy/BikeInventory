4b72536ecf4804c8c2214f8a41882db029f93608
masudbappy <masud.bappy90@gmail.com>
2018-12-12 17:13:03 +0600

    First Comiit

---
+++ src/main/java/com/masudbappy/bike/models/Bike.java
@@ -0,0 +1,79 @@
+package com.masudbappy.bike.models;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Bike {
+    private String name;
+    private String email;
+    private String phone;
+    private String model;
+    private String serialNumber;
+    private BigDecimal purchagePrice;
+    private Date purchaseDate;
+    private boolean contact;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public BigDecimal getPurchagePrice() {
+        return purchagePrice;
+    }
+
+    public void setPurchagePrice(BigDecimal purchagePrice) {
+        this.purchagePrice = purchagePrice;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public boolean isContact() {
+        return contact;
+    }
+
+    public void setContact(boolean contact) {
+        this.contact = contact;
+    }
+}
